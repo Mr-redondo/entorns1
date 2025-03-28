057a88d268340e1d50e5b3c0e9afff86c3a7e81d
rredondo <163598876+Mr-redondo@users.noreply.github.com>
2025-03-28 15:39:51 +0100

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 
 # Configuration
-GITHUB_TOKEN = os.getenv("")  # Set this as an environment variable
+GITHUB_TOKEN = "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"  # Set this as an environment variable
 GITHUB_REPO = "Mr-redondo/randomC"
 
 # Generate dynamic release tag and name based on date/time
@@ -12,7 +12,7 @@ current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 RELEASE_TAG = f"v{current_time}"
 RELEASE_NAME = f"Version {current_time}"
 RELEASE_BODY = "Automated release with timestamp."
-FILE_PATH = "/var/lib/jenkins/workspace/Compile_Random_C/main"  # Update with your actual file path
+FILE_PATH = "/home/ubuntu22/main.c"  # Update with your actual file path
 
 # GitHub API URL
 GITHUB_API_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases"
